bb9bc0432fc60b537896dc8dc24bc4f273357af9
artem <fatalcounter@ya.ru>
2022-03-18 14:46:44 +0100

    saving commit

--- client/src/profile-pic.test.tsx
+++ client/src/profile-pic.test.tsx
@@ -2,12 +2,11 @@ import ProfilePic from "./profile-pic";
 
 import { render } from "@testing-library/react";
 
-test("when url is not passed it ends up default", () => {
-    const { container } = render(<ProfilePic />);
-    expect(
-        container
-            .querySelector("img")
-            .src.endsWith("defaultProfilePic.jpg")
-            .toBe(true)
+it("when url is not passed it ends up default", () => {
+    const { container } = render(
+        <ProfilePic first={"first"} last="last" showUploader={() => {}} />
     );
+    expect(
+        container.querySelector("img").src.endsWith("/defaultProfilePic.jpg")
+    ).toBe(true);
 });
